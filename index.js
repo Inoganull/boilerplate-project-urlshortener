87bdd95124a1747fb3483a14befcc0bfed697b4c
Kyaw Min Hein <kyawminhein101@gmail.com>
2025-06-24 22:37:24 +0700

    URL shortener

--- index.js
+++ index.js
@@ -19,6 +19,59 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+const bodyParser = require('body-parser');
+const dns = require('dns');
+const urlParser = require('url');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+let urlDatabase = {};
+let urlCount = 1;
+
+// POST endpoint to shorten URL
+app.post('/api/shorturl', (req, res) => {
+  const original_url = req.body.url;
+
+  // Check for protocol
+  if (!/^https?:\/\/.+/i.test(original_url)) {
+    return res.json({ error: 'invalid url' });
+  }
+
+  let parsedUrl;
+  try {
+    parsedUrl = urlParser.parse(original_url);
+  } catch {
+    return res.json({ error: 'invalid url' });
+  }
+
+  // Check for valid hostname
+  if (!parsedUrl.hostname) {
+    return res.json({ error: 'invalid url' });
+  }
+
+  dns.lookup(parsedUrl.hostname, (err) => {
+    if (err) {
+      return res.json({ error: 'invalid url' });
+    } else {
+      const short_url = urlCount++;
+      urlDatabase[short_url] = original_url;
+      res.json({ original_url, short_url });
+    }
+  });
+});
+
+// GET endpoint to redirect to original URL
+app.get('/api/shorturl/:short_url', (req, res) => {
+  const short_url = req.params.short_url;
+  const original_url = urlDatabase[short_url];
+  if (original_url) {
+    res.redirect(original_url);
+  } else {
+    res.json({ error: 'No short URL found for the given input' });
+  }
+});
+
+
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
